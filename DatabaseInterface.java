2f11bd2daf3ea0bdfc68f18fb173ef9648fdedea
DevikaGangaRani <84277501+DevikaGangaRani@users.noreply.github.com>
2024-03-27 11:45:16 +0530

    Delete DatabaseInterface.java

--- DatabaseInterface.java
+++
@@ -1,7 +0,0 @@
-// iCRUDS interface
-
-interface iCRUDS
-{
-	int insertRecord(String[] pRecordDetails, String pTableName) throws Exception;
-	String[][] loadRecords(String pTableName) throws Exception;
-}
